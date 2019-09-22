474220c4b28ca259b6bbd8c962de6509dbe152c3
seon <seonu0823@gmail.com>
2019-09-22 17:01:54 +0900

    #9-9 Deleting To Dos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useReducer, useState } from 'react';
+import uuid from 'uuid/v4';
 
 const initialState = {
     toDos : []
 };
 
 const ADD = 'add';
+const DEL = 'del';
 
 const reducer = (state, action) => {
     switch (action.type) {
         case ADD : 
-            return { toDos : [...state.toDos, { text : action.payload }] };             // anti mutation
+            return { toDos : [...state.toDos, { id : uuid(), text : action.payload }] };             // anti mutation
+
+        case DEL : 
+            return { toDos : state.toDos.filter(toDo => toDo.id !== action.payload) };
 
         default : 
             return;
@@ -23,6 +28,7 @@ function App() {
         e.preventDefault();
 
         dispatch({ type : ADD, payload : newToDo });
+        setNewToDo('');
     };
     const onChange = e => {
         const { target : { value } } = e;
@@ -38,7 +44,12 @@ function App() {
             </form>
             <ul>
                 <h2>To Dos</h2>
-                { state.toDos.map((toDo, index) => <li key={ index }>{ toDo.text }</li>) }
+                { state.toDos.map((toDo) => (
+                    <li key={ toDo.id }>
+                        <span>{ toDo.text }</span>
+                        <button onClick={ () => dispatch({ type : DEL, payload : toDo.id }) }>❌</button>
+                    </li>
+                )) }
             </ul>
         </>
     );
